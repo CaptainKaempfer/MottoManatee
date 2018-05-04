2468c3a4180580270e0ed60a36ac2892c1f03258
Yoshiiiiiii <36034123+Yoshiiiiiii@users.noreply.github.com>
2018-05-04 08:54:23 +0200

    get user by nickname
    added a prototype function of get user by nickname

--- ManateeMottoExtension/src/com/mottomanatee/neo4j/ServerExtension.java
+++ ManateeMottoExtension/src/com/mottomanatee/neo4j/ServerExtension.java
@@ -9,27 +9,30 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.codehaus.jackson.map.ObjectMapper;
-import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.*;
 
-@Path( "/ServerExtension")
-public class ServerExtension 
-{
-	private final GraphDatabaseService graphdb; 
+@Path("/ServerExtension")
+public class ServerExtension {
+	private final GraphDatabaseService graphdb;
 	private final ObjectMapper objectMapper;
-	
-	public ServerExtension( @Context GraphDatabaseService graphDb)
-    {
-        this.graphDb = graphDb;
-        this.objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(JsonMethod.FIELD, Visibility.ANY);
-    }
-	
-//	@POST
-//	@Produces(MediaType.APPLICATION_JSON)
-//	@Path("/users/new/")
-	
-	 @GET
-	 @Path("/{personName}")
-	 
-	
+
+	public ServerExtension(@Context GraphDatabaseService graphdb) {
+		this.graphdb = graphdb;
+		this.objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(JsonMethod.FIELD, Visibility.ANY);
+	}
+
+	// @POST
+	// @Produces(MediaType.APPLICATION_JSON)
+	// @Path("/users/new/")
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("users/{nick}")
+	public Response findUserByNick(@PathParam("nick") String nick) throws Exception {
+		Node node = graphdb.getNodeById(1);
+		String str = this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(node);
+		return Response.ok(str, MediaType.APPLICATION_JSON).build();
+	}
+
 }

a3114b203dd5faf21871a2f912503e746b93684e
Ne0nKnight <37457898+Ne0nKnight@users.noreply.github.com>
2018-05-18 12:01:46 +0200

    Fehlerbehebung und neue requests

--- query_utils.js
+++ query_utils.js
@@ -1,20 +1,39 @@
 /* Query utility functions for Web Engineering REST Project*/
 
-//GET JSON data for users or mottos and return a JSON Object
-function getJSON(userID = "", mottoID = ""){
+//GET JSON data for users and return a JSON Object
+function getUserData(userID = "", callback){
 	var req = new XMLHttpRequest();
-	if((userID == "") && (mottoID == "")){
+	if( userID == ""){ //all users
 		req.open("GET", "https://mottomanatee.firebaseio.com/api/users" +".json", true);
-	}else if((userID != "") && (mottoID == "")){
+	}else{ // single user
 		req.open("GET", "https://mottomanatee.firebaseio.com/api/users/" + userID + ".json", true);
-	}else if((userID != "") && (mottoID != "")){
-		req.open("GET", "https://mottomanatee.firebaseio.com/api/users/" + userID + "/" + mottoID +".json", true);
 	}
     
 	req.onload = function () {
 		if (req.readyState == 4 && req.status == "200") {
             var responseObj = JSON.parse(req.responseText); //JSON object erstellen un zurückgeben
-            return responseObj;
+            callback(responseObj);
+		}
+	}
+	
+	req.send();
+}
+
+//GET JSON data for mottos and return a JSON Object
+function getMottoData(userID = "", mottoID = "", callback){
+	var req = new XMLHttpRequest();
+	if((userID != "") && (mottoID == "")){ //all mottos for a given user
+		req.open("GET", "https://mottomanatee.firebaseio.com/api/mottos" + '.json?orderBy="user"&equalTo="' + userID + '"', true);
+	}else if((userID == "") && (mottoID != "")){ // single motto
+		req.open("GET", "https://mottomanatee.firebaseio.com/api/mottos/" + mottoID + ".json", true);
+	}else if((userID == "") && (mottoID = "")){// 5 newest mottos
+		req.open("GET", 'https://mottomanatee.firebaseio.com/api/mottos.json?orderBy="timestamp"&limitToLast=5', true);
+	}
+
+	req.onload = function () {
+		if (req.readyState == 4 && req.status == "200") {
+            var responseObj = JSON.parse(req.responseText); //JSON object erstellen un zurückgeben
+            callback(responseObj);
 		}
 	}
 	
@@ -29,14 +48,16 @@ function createUser(jsonObj){
 	req.send(jsonString);
 }
 
-//Create new motto from JSON data
-function postMotto(jsonObj){
+//Create new motto from JSON data and return the mottoID as a String
+function postMotto(jsonObj,callback){
 	var jsonString = JSON.stringify(jsonObj);
 	var req = new XMLHttpRequest();
 	req.open("POST", "https://mottomanatee.firebaseio.com/api/mottos" + ".json", true);
 	req.onload = function () {
 		if (req.readyState == 4 && req.status == "200") {
-			document.write(req.responseText); // mottoID in der Form {"name":"-JSOpn9ZC54A4P4RoqVa"}
+			var jsonObj = JSON.parse(req.responseText); // mottoID in der Form {"name":"-JSOpn9ZC54A4P4RoqVa"}, muss irgendwo im html auf der webseite hinterlegt sein
+			var mottoID = jsonObj.name;
+			callback(mottoID);
 		}
 	}
 	req.send(jsonString);

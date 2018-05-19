d37e0455c3731ccc2f3ec40798b27f2b3c862f7f
Maik <38942516+FenikkKytt@users.noreply.github.com>
2018-05-19 16:53:46 +0200

    Sprint 3
    - Edit Profile
    - Delete Mottos
    - LogIn Validation and Modal integration
    - Registration Validation and Modal intergration
    - Motto Slider added

--- WebEngi_Frontend/views/register/register_controller.js
+++ WebEngi_Frontend/views/register/register_controller.js
@@ -5,11 +5,77 @@ function RegisterCtrl ($rootScope, $scope, $cookies, DataInterchangeService, Mod
 
 	$scope.message = 'Keine Registrierungsdaten gefunden';
 
+	// Register Declarations
+	$scope.user = {
+		firstName: '',
+		lastName: '',
+		username: '',
+		email: '',
+		address: '',
+		city: '',
+		state: '',
+		postalCode: '',
+		password: ''
+	};
+	
+	$scope.states = ('Baden-Württember Bayern Berlin Brandenburg Bremen Hamburg Hessen Mecklenburg-Vorpommern Niedersachsen ' +
+	'Nordrhein-Westfalen Rheinland-Pfalz Saarland Sachsen Sachsen-Anhalt Schleswig-Holstein ' +
+	'Thüringen').split(' ').map(function(state) {
+		return {abbrev: state};
+	});
+
 	/////////////////////////////////// Modals //////////////////////////////////////////
 	/**
 	 * Opens a standard modal 
 	 */
-	$scope.openLogin = function () {
+	$scope.openRegister = function () {
+
+		// Additional simple form validation
+		if($scope.user.firstName == '')
+		{
+			alert("Bitte geben Sie einen gültigen Vornamen ein!"); 
+			return false;
+		}
+			
+		if($scope.user.lastName == '')
+		{
+			alert("Bitte geben Sie einen gültigen Nachnamen ein!"); return false;
+		}
+
+		if($scope.user.username == '')
+		{
+			alert("Bitte geben Sie einen gültigen Benutzernamen ein!");
+			return false;
+		}	
+
+		if($scope.user.email == '')
+		{
+			alert("Bitte geben Sie eine gültige E-Mail Adresse ein!"); 
+			return false;
+		}
+			
+		if($scope.user.address == '')
+		{
+			alert("Bitte geben Sie eine gültige Adresse ein!"); 
+			return false;
+		}
+			
+		if($scope.user.city == '')
+		{
+			alert("Bitte geben Sie eine gültige Stadt ein!"); 
+			return false;
+		}
+			
+		if($scope.user.password == '')
+		{
+			alert("Bitte geben Sie ein gültiges Passwort ein!"); 
+			return false;
+		}
+
+		// Save data in firebase database
+
+
+		// Open user respond dialog
 		ModalService.openRegisterModal(function(){
 			$rootScope.isSessionCookie = true;
 		});

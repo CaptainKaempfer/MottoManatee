2fc403d8a9f265539d5faf1999801972bf0d7793
Yoshiiiiiii <36034123+Yoshiiiiiii@users.noreply.github.com>
2018-05-24 12:19:29 +0200

    proper formatting

--- WebEngi_Frontend/views/register/register_controller.js
+++ WebEngi_Frontend/views/register/register_controller.js
@@ -72,31 +72,32 @@ function RegisterCtrl ($rootScope, $scope, $cookies, DataInterchangeService, Mod
 			return false;
 		}
 		var email = $scope.user.email;
-      var password = $scope.user.password;
-      if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-      }
-      if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-      }
-      // Sign in with email and pass.
-      // [START createwithemail]
-      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		var password = $scope.user.password;
+		if (email.length < 4) {
+			alert('Please enter an email address.');
+			return;
+		}
+		if (password.length < 4) {
+			alert('Please enter a password.');
+			return;
+		}
+		// Sign in with email and pass.
+		// [START createwithemail]
+		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // [START_EXCLUDE]
         if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
-        } else {     
-          alert(errorMessage);
+			alert('The password is too weak.');
+        } 
+		else {     
+			alert(errorMessage);
         }
         console.log(error);
         // [END_EXCLUDE]
-      });
-      // [END createwithemail]
+		});
+		// [END createwithemail]
 
 		// Save data in firebase database
 

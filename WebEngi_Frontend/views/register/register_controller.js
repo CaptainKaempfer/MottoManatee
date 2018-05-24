08955859ee77a9c12ec84feae142f50877e41bf0
Yoshiiiiiii <36034123+Yoshiiiiiii@users.noreply.github.com>
2018-05-24 12:13:49 +0200

    registration added to reg..controller

--- WebEngi_Frontend/views/register/register_controller.js
+++ WebEngi_Frontend/views/register/register_controller.js
@@ -71,6 +71,32 @@ function RegisterCtrl ($rootScope, $scope, $cookies, DataInterchangeService, Mod
 			alert("Bitte geben Sie ein gültiges Passwort ein!"); 
 			return false;
 		}
+		var email = $scope.user.email;
+      var password = $scope.user.password;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      // [START createwithemail]
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {     
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END createwithemail]
 
 		// Save data in firebase database
 

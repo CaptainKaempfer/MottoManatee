1090ae813eb72564bf06fef2df13ea17e0ab01a2
CaptainKaempfer <38939019+CaptainKaempfer@users.noreply.github.com>
2018-06-06 19:20:25 +0200

    dev nik

--- WebEngi_Frontend/views/showMotto/showMotto_controller.js
+++ WebEngi_Frontend/views/showMotto/showMotto_controller.js
@@ -126,7 +126,7 @@ function ShowCtrl ($rootScope, $scope, $cookies, DataInterchangeService, ModalSe
 
     $scope.moreFunction = function () {
 		
-		ModalService.openLoginModel(function(){
+		ModalService.openDetailModal(function(){
 			$rootScope.isSessionCookie = true;
 		});
 	};
